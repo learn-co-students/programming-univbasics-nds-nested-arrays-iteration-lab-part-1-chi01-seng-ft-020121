1a621779c58f56be5a704356bacd8fe25e081dd9
Val_1 S_z <csa.aiden@gmail.com>
2021-01-25 01:54:46 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,6 +1,18 @@
-def find_even_values(src)
-  # Given an Array of Arrays of Integers ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] ):
-  #
-  # Output all even values in each nested array
+def join_nested_strings(src)
+  # src will be an Array of Arrays of Strings and Integers
+  # Combine all Strings present in the AoA into a single value and return it
 
+  row_index = 0 
+  new_src = []
+  while row_index < src.count do
+    element_index = 0 
+    while element_index < src[row_index].count do
+      if src[row_index][element_index].is_a? String 
+        new_src << src[row_index][element_index]
+      end
+      element_index += 1 
+    end
+    row_index += 1 
+  end
+  new_src.join(" ")
 end
--- spec/intro_to_ruby_hashes_lab_spec.rb
+++ spec/intro_to_ruby_hashes_lab_spec.rb
@@ -1,34 +1,22 @@
 require 'spec_helper'
 
-array_1 = [[1,2,3], [4,5,6], [7,8,9]]
-array_2 = [[-1, -900], [10, 31], [5, 0], [14, -41], [1, 19]]
-array_3 = [[86262, 58115], [22558, 97709], [66236, 28056], [25992, 81375], [11248, 56789], [42783, 27353], [70796, 63432], [53234, 63058], [72316, 8675309]]
+mixed_data_1 = [
+  ["The", 4, "quick"],
+  [-1, "brown", "fox", 30],
+  ["studied", 101, 233, "Ruby"]
+]
 
+mixed_data_2 = [
+  ["Turning", "and", "turning", "in", "the", "widening", "gyre"],
+  ["The", "falcon", "cannot", "hear", "the", "falconer;"],
+  ["Things", "fall", "apart;", "the", "centre", "cannot", "hold"]
+]
 
-describe "Write a method called \"find_even_values\"." do
-  describe "that when given an Array of Arrays of numbers," do
-    it "outputs only the even numbers in each nested array" do
-      expect{find_even_values(array_1)}.to output(/2/).to_stdout
-      expect{find_even_values(array_1)}.to output(/4/).to_stdout
-      expect{find_even_values(array_1)}.to output(/6/).to_stdout
-      expect{find_even_values(array_1)}.to output(/8/).to_stdout
-
-      expect{find_even_values(array_2)}.to output(/-900/).to_stdout
-      expect{find_even_values(array_2)}.to output(/10/).to_stdout
-      expect{find_even_values(array_2)}.to output(/0/).to_stdout
-      expect{find_even_values(array_2)}.to output(/14/).to_stdout
-      
-      expect{find_even_values(array_3)}.to output(/86262/).to_stdout
-      expect{find_even_values(array_3)}.to output(/22558/).to_stdout
-      expect{find_even_values(array_3)}.to output(/66236/).to_stdout
-      expect{find_even_values(array_3)}.to output(/28056/).to_stdout
-      expect{find_even_values(array_3)}.to output(/25992/).to_stdout
-      expect{find_even_values(array_3)}.to output(/11248/).to_stdout
-      expect{find_even_values(array_3)}.to output(/70796/).to_stdout
-      expect{find_even_values(array_3)}.to output(/63432/).to_stdout
-      expect{find_even_values(array_3)}.to output(/53234/).to_stdout
-      expect{find_even_values(array_3)}.to output(/63058/).to_stdout
-      expect{find_even_values(array_3)}.to output(/72316/).to_stdout
+describe "Write a method called \"join_nested_strings\"" do
+  describe "that takes in an Array of Arrays containing Integers and Strings" do
+    it "returns a single combined string of all strings in the data structure" do
+      expect(join_nested_strings(mixed_data_1)).to match(/The quick brown fox studied Ruby/)
+      expect(join_nested_strings(mixed_data_2)).to match(/Turning and turning in the widening gyre The falcon cannot hear the falconer; Things fall apart; the centre cannot hold/)
     end
   end
 end
